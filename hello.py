670d515d909350bbd654d147e03bfb2de4433e23
Eliot Logan <eliot.logan@gmail.com>
2018-03-31 11:27:20 -0700

    ok

---
+++ hello.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+#from flask.ext.images import resized_img_src
+
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'monkey'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/eliot', methods=['GET', 'POST'])
+def dummy():
+    return render_template('uploader.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template('uploader.html')
+
+@app.route('/uploads/<filename>', methods=['GET', 'POST'])
+def uploaded_file(filename):
+    '''
+    render_template('render_image.html', image_name=filename)
+    '''
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
