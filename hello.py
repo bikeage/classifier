78f33dbf6dd98823ae5975e5f5b52d5b61fdb281
Eliot Logan <eliot.logan@gmail.com>
2018-04-01 08:01:15 -0700

    mostly updates to templates

--- hello.py
+++ hello.py
@@ -1,23 +1,29 @@
+"""This is a flask fron end for an image classifier, Resnet50 at first.
+to be replaced with a custom neural net once I have the time.
+"""
 import os
+
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
-#from flask.ext.images import resized_img_src
 
 UPLOAD_FOLDER = 'uploads/'
-ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'monkey'
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/eliot', methods=['GET', 'POST'])
 def dummy():
     return render_template('uploader.html')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -39,10 +45,9 @@ def upload_file():
                                     filename=filename))
     return render_template('uploader.html')
 
+#@app.route('/view', methods=['GET', 'POST'])
 @app.route('/uploads/<filename>', methods=['GET', 'POST'])
-def uploaded_file(filename):
-    '''
-    render_template('render_image.html', image_name=filename)
-    '''
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+def uploaded_file( filename ):
+    return render_template('render_image.html', filename='uploads/'+filename)
+    #return send_from_directory(app.config['UPLOAD_FOLDER'],
+    #                           filename)
