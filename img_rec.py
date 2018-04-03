7523b1722211c91c8bca0e24e0b13357bebd740d
Eliot Logan <eliot.logan@gmail.com>
2018-04-03 10:43:44 -0700

    commented out verbose output

--- img_rec.py
+++ img_rec.py
@@ -8,12 +8,15 @@ model = resnet50.ResNet50()
 
 def get_predictions(pict):
     # given an image, runs through ResNet50, and returns the top 5 results
-    img = image.load_img(pict, target_size=(224, 224))
+    img = image.load_img(pict, target_size=(224,224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = resnet50.preprocess_input(x)
     predictions = model.predict(x)
     predicted_classes = resnet50.decode_predictions(predictions, top=5)
+    str = ""
     print("This is an image of:")
     for imagenet_id, name, likelihood in predicted_classes[0]:
-        print(" - {}: {:2f} likelihood".format(name, likelihood))
+        #print(" - {}: {:2f} likelihood".format(name, likelihood))
+        str += (" - {}: {:2f} likelihood".format(name, likelihood))
+    return str
